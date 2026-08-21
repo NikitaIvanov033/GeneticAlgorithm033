898cc182d4b3d4f5d1b5d54e6d1809dcc2edede5
NikitaIvanov033 <mainuser033@gmail.com>
2026-08-21 20:36:22 +0700

    Protocol doesn't require abstract methods

--- core/individual.py
+++ core/individual.py
@@ -1,23 +1,18 @@
 from typing import Protocol, TypeVar, Tuple
-from abc import abstractmethod
 
 T = TypeVar('T', bound='Individual', covariant=True)
 
 
 class Individual(Protocol):
 
-    @abstractmethod
     def copy(self: T) -> T:
         ...
 
-    @abstractmethod
     def mutate(self, rate: float) -> None:
         ...
 
-    @abstractmethod
     def crossover(self: T, other: T) -> Tuple[T, T]:
         ...
 
-    @abstractmethod
     def __len__(self) -> int:
         ...
